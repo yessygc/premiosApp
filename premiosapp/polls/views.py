fe9b786fba65552ddf506363abf877930bc28bbf
yessygc <yessgarciacampos@gmail.com>
2022-07-14 01:36:56 -0500

    creando vistas para la aplicacion

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Estas en la página principal de Premios Platzi App")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta número {question_id}")
