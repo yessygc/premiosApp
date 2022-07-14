80da04ed71410a6553b155e1dde1902a1a63cd9b
yessygc <yessgarciacampos@gmail.com>
2022-07-14 17:51:43 -0500

    Agregando html y utilizando la etiqueta url

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Estas en la página principal de Premios Platzi App")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 
 def results(request, question_id):
