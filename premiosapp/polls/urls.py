4acd96223710802678d4cba8fc73af12c29cf82f
yessygc <yessgarciacampos@gmail.com>
2022-07-19 04:06:35 -0500

    Formularios, creando las vistas vote y results

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #ex: /polls/
     path("", views.index, name="index"),
     #ex: /polls/5/
-    path("<int:question_id>/detail/estaeslamejorpagina", views.detail, name="detail"),
+    path("<int:question_id>/detail/", views.detail, name="detail"),
     #ex: /polls/5/results
     path("<int:question_id>/results/", views.results, name="results"),
     #ex: /polls/5/vote
